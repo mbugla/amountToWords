20f84e30d8e95c4dc9e07b2dfbb7f7f86f3485bf
Marek Bugla <marek.bugla@creativestyle.pl>
2016-12-06 12:12:10 +0100

    added test for units values

--- src/Lib/AmountToWords.php
+++ src/Lib/AmountToWords.php
@@ -6,8 +6,13 @@ namespace Lib;
 
 class AmountToWords
 {
+    /**
+     * @param float $amount
+     * @param string $currency
+     * @return string
+     */
     public function convert(float $amount, string $currency): string
     {
-
+        return "zero ".$currency;
     }
 }
--- src/Test/Lib/AmountToWordsTest.php
+++ src/Test/Lib/AmountToWordsTest.php
@@ -8,13 +8,49 @@ use Lib\AmountToWords;
 
 class AmountToWordsTest extends \PHPUnit_Framework_TestCase
 {
+    const CURRENCY = 'PLN';
+
+    /** @var AmountToWords */
+    private $converter;
+
+    public function setUp()
+    {
+        $this->converter = new AmountToWords();
+    }
     /**
      * @test
      */
     public function returnZeroForEmptyAmount()
     {
-        $converter = new AmountToWords();
-        $currency = 'PLN';
-        $this->assertEquals('zero '.$currency, $converter->convert(0.00, $currency));
+        $this->assertSame('zero '.self::CURRENCY, $this->converter->convert(0.00, self::CURRENCY));
+    }
+
+    /**
+     * @test
+     * @dataProvider getUnits
+     * @param $units
+     * @param $expectedResult
+     */
+    public function returnProperAmountStringForUnits($units, $expectedResult)
+    {
+        $this->assertSame($expectedResult.' '.self::CURRENCY, $this->converter->convert($units, self::CURRENCY));
+    }
+
+    /**
+     * @return array
+     */
+    public function getUnits()
+    {
+        return [
+            [1.00, 'Jeden'],
+            [2.00, 'Dwa'],
+            [3.00, 'Trzy'],
+            [4.00, 'Cztery'],
+            [5.00, 'Pięć'],
+            [6.00, 'Sześć'],
+            [7.00, 'Siedem'],
+            [8.00, 'Osiem'],
+            [9.00, 'Dziewięć'],
+        ];
     }
 }
